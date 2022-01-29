61c81d7e0ed736d11de1e81331e67f354cc09ffa
Rajen Shrestha <rajen.shrestha@argenti.com.au>
2022-01-29 17:27:20 +1100

    Added asp.net gRPC Service and added proto file on seperate project

---
+++ Rs.Net.Core.GrpcService/Program.cs
@@ -0,0 +1,19 @@
+using Rs.Net.Core.GrpcService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+// https://docs.microsoft.com/en-us/aspnet/core/grpc/aspnetcore?view=aspnetcore-6.0&tabs=visual-studio#configure-grpc
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// https://docs.microsoft.com/en-us/aspnet/core/grpc/aspnetcore?view=aspnetcore-6.0&tabs=visual-studio#configure-grpc
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
