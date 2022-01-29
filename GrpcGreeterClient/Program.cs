0b46f7edf6326dac742930afc341e5d2f503cbe5
Rajen Shrestha <rajen.shrestha@argenti.com.au>
2022-01-29 17:58:53 +1100

    Added client to test

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7240");
+var client = new Rs.Net.Core.MyGRPC.Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new Rs.Net.Core.MyGRPC.HelloRequest
+{
+    Name = "Rajen"
+});
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
